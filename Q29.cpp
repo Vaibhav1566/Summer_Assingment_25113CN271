1510b1ab503490446f6b4a34835f9f1de63243af
Vaibhav1566 <vaibhav26062006@gmail.com>
2026-06-11 22:33:40 +0530

    Add files via upload

---
+++ Q29.cpp
@@ -0,0 +1,15 @@
+// HALF STAR PYRAMID 
+
+#include <iostream>
+using namespace std;
+int main(){
+    int n;
+    cout << "Enter number of Row = ";
+    cin>> n;
+    for(int i=1 ;i<=n;i++){
+        for (int j=1 ;j<=i;j++){
+            cout <<"*";
+        } 
+        cout << endl;
+    }
+}
